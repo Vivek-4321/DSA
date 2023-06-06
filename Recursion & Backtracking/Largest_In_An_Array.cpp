1f96c6b1cf64c8e9828af5855b1d5bb8a3da925f
Vivek-4321 <vivekvenugopal514@gmail.com>
2023-06-06 20:48:41 +0530

    New Problem Solved.....

---
+++ Recursion & Backtracking/Largest_In_An_Array.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Recursive function
+int largest_element(int n, int arr[]){
+
+   if(n==1) return arr[0];  // Base condition
+
+   return max(arr[n-1], largest_element(n-1, arr));
+}
+
+//Driver code
+ int main(){ 
+
+    int arr[] = {10, 45, 78, 34, 67};
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    cout<<"Largest Element is : "<<largest_element(n, arr); 
+
+}
